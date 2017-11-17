71b1149eb8bde9b1d4f3a3892fdc1bc56b5d7bf1
Konstantin BIFERT <hello@kissu.io>
2017-11-17 03:19:54 +0100

    temporary basic routes and user controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'users#new'
+  post 'users', to: 'users#create'
+  get 'requests/thanks', to: 'requests#thanks'
+  resources :requests, only: [:index, :show, :edit, :update]
 end
