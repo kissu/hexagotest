92c5ae93fb487f87ffd84e7c51ec83bc8d1eb1b5
Konstantin BIFERT <hello@kissu.io>
2017-11-18 10:30:30 +0100

    set the timezone to paris

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,8 @@ module Hexagotest
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.time_zone = 'Paris'
+    config.active_record.default_timezone = :local
   end
 end
