e2ee880892cdda3f1b2ad354652ea581999d910f
Konstantin BIFERT <hello@kissu.io>
2017-11-20 05:39:39 +0100

    minor PR changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,10 @@ puts 'All stuff destroyed.'
   biography: Faker::Lorem.sentence(5, true, 10), confirmation_token: nil)
 end
 
-# User.create!(name: 'kos', email: 'hello@kissu.io', password: 'hexa',
-#   phone_number: '0101010101', biography: 'whateverzerzeiorjze',
-#   confirmation_token: nil)
-
 User.last(5).first.update(wait_order: 1)
 User.last(4).first.update(wait_order: 2)
 User.last(3).first.update(wait_order: 3)
 User.last(2).first.update(wait_order: 4)
 User.last.update(wait_order: 5)
-
-# Request.find(10).update(status: 0)
 
 puts 'Seed generated !'
